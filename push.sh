5a4d7bf9c86fbf359879147b46652ee79648daf1
daixiongsheng <daixiongsheng@gmail.com>
2021-04-16 09:48:45 +0800

    2021-04-16-09:48:45

--- push.sh
+++ push.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 git config user.email daixiongsheng@gmail.com
 git add .
-m=$1
 date=$(date "+%Y-%m-%d-%H:%M:%S")
-git commit -m "$date $m"
+git commit -m "$date $1"
 git push origin master
